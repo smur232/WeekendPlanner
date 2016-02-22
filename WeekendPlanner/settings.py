1114d25b2ee85d6398c0250062cf27970d3a040f
smur232 <sarah.d.murata@gmail.com>
2016-02-23 12:44:44 +1300

    added map app

--- WeekendPlanner/settings.py
+++ WeekendPlanner/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eh$mzu==k3w3+%ne8@!i7#9++iu#_+l5ebom2#z6k6rf*wzf*+'
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +41,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crawler.apps.CrawlerConfig',
     'map.apps.MapConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -71,14 +82,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'WeekendPlanner.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
     }
 }
 
--- map/views.py
+++ map/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from crawler.models import Event
+from WeekendPlanner.settings import get_env_variable
+
+MAPS_KEY = get_env_variable('GOOGLE_MAPS_KEY')
 
 
-def index(request):
-    context = {}
+def index(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    context = {'event': event, 'MAPS_KEY': MAPS_KEY}
     return render(request, 'map/index.html', context)
